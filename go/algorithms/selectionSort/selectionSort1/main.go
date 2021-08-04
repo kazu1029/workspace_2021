b20aa22a6e26e103d2972c345061e50099c1ed87
kazu1029 <imgnt10nxqu05@gmail.com>
2021-08-04 22:17:30 +0900

    Linked List and Selection Sort

---
+++ go/algorithms/selectionSort/selectionSort1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// selectionSort sorts int slice by iterating and finding minimum value, then exchanging it and the first value.
+// inputted slice is sorted.
+func selectionSort(items []int) {
+	var n = len(items)
+	for i := 0; i < n; i++ {
+		var minIdx = i
+		for j := 0; i < n-1; i++ {
+			if items[j] < items[minIdx] {
+				minIdx = j
+			}
+		}
+		items[i], items[minIdx] = items[minIdx], items[i]
+	}
+}
+
+func main() {
+	slice := generateIntSlice(20)
+	fmt.Println("\n--- Unsorted ---\n\n", slice)
+	selectionSort(slice)
+	fmt.Println("\n--- Sorted ---\n\n", slice)
+}
+
+func generateIntSlice(size int) []int {
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
